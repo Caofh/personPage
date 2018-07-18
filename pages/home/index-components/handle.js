535a7614d13bd1e0f4bd4fba5abceffd46146841
caofanghui <caofanghui@vipkid.com.cn>
2018-07-18 19:29:41 +0800

    Update-资源按照时间戳排序.

--- pages/home/index-components/handle.js
+++ pages/home/index-components/handle.js
@@ -6,20 +6,18 @@ function handle (data, obj) {
   console.log(data)
 
   let data_new = data.original.data || []
+
+  // 处理数据
   data_new.map((item) => {
     item.timestamp_str = moment(parseInt(item.timestamp)).format('YY-MM-DD HH:mm:ss')
   })
 
+  // 按照时间戳排序
+  data_new.sort((a, b) => {
+    return a.timestamp < b.timestamp
+  })
 
   console.log(data_new)
-
-
-
-
-
-
-
-
 
 }
 
